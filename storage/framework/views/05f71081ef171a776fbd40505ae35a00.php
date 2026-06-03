72d845624cf4b2544025a4d032730f3780256974
Rodrigo Silva <rodrigo.1704200@gmail.com>
2026-06-03 16:36:29 +0100

    .

--- storage/framework/views/05f71081ef171a776fbd40505ae35a00.php
+++
@@ -1,7 +0,0 @@
-
-
-<ui-close data-flux-modal-close <?php echo e($attributes); ?>>
-    <?php echo e($slot); ?>
-
-</ui-close>
-<?php /**PATH C:\laragon\www\PROJETO\vendor\livewire\flux\src/../stubs/resources/views/flux/modal/close.blade.php ENDPATH**/ ?>
