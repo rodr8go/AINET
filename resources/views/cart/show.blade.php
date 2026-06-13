48b06a60f3f5edfe18d2565342e80c110d0ef828
Ricardo Arsénio <Rickyflipsk88@hotmail.com>
2026-06-13 19:21:12 +0100

    Fix cart button visibility

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -127,8 +127,8 @@
                     </div>
 
                     <div class="mt-6">
-                        @can('confirm-cart')
                         @auth
+                            @can('confirm-cart')
                             <div>
                                 <flux:button href="{{ route('checkout.index') }}" 
                                              variant="filled" 
@@ -137,6 +137,7 @@
                                     Finalizar Encomenda
                                 </flux:button>
                             </div>
+                            @endcan
                         @else
                             <div class="space-y-3">
                                 <flux:button href="{{ route('login') }}" 
@@ -150,7 +151,7 @@
                                 </p>
                             </div>
                         @endauth
-                        @endcan
+                        
                     </div>
                 </flux:card>
 
